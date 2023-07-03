c18d564309439a3a51a74c59476a5bae560e9b6a
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-07-03 17:56:28 +0300

    Add files via upload
    exam_average files uploaded

---
+++ 03_exam_average/src/exam_average/average.java
@@ -0,0 +1,16 @@
+package exam_average;
+
+public class average {
+
+	public static void main(String[] args) {
+		int exam1 = 76;
+		int exam2 = 85;
+		int exam3 = 60;
+		
+		int sonuc = (exam1+exam2+exam3)/3;
+		
+		System.out.println("your exam average is:" + sonuc);
+
+	}
+
+}
