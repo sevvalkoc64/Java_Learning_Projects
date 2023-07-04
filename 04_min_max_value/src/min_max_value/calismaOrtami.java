cc35ecba749d410042ace09059ccaf7e2c08bfaa
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-07-04 11:22:48 +0300

    Add files via upload
    min_max_value files uploaded

---
+++ 04_min_max_value/src/min_max_value/calismaOrtami.java
@@ -0,0 +1,14 @@
+package min_max_value;
+
+public class calismaOrtami {
+
+	public static void main(String[] args) {
+		long maxDeger= Long.MAX_VALUE;
+		long minDeger= Long.MIN_VALUE;
+		
+		System.out.println(maxDeger);
+		System.out.println(minDeger);
+
+	}
+
+}
