aa38326a619f7bca8a96a8fea4250364e600d5a2
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-07-08 20:47:42 +0300

    even number files uploaded

---
+++ 07_even_number/src/even.java
@@ -0,0 +1,14 @@
+
+public class even {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println("the even numbers: " +i);
+			}
+		}
+
+	}
+
+}
