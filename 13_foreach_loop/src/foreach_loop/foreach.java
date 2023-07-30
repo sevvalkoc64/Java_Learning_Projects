9be6111b1cd2425cdfef84b63e1ae8ccbbe34556
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-07-30 20:35:31 +0300

    Add files via upload

---
+++ 13_foreach_loop/src/foreach_loop/foreach.java
@@ -0,0 +1,15 @@
+package foreach_loop;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		String[] isimler= {"ahmet", "ayse", "hasan", "mehmet"};
+		
+		for (String a : isimler) {
+			System.out.println(a);
+			
+		}
+
+	}
+
+}
