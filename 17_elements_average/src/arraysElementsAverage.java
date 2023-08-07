d39b96702ec4aefc2b1faa08e2b0f1e30db4dc0f
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-08-07 13:17:49 +0300

    Add files via upload

---
+++ 17_elements_average/src/arraysElementsAverage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arraysElementsAverage {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		
+		int i,sum;
+		double average=0;
+		int[] array= new int[4];
+		
+		for(i=0;i<=array.length;i++) {
+			System.out.println("enter "+i+"."+"element of the array:");
+			array[i]=scanner.nextInt();
+			System.out.println("array's "+i+" element: "+array[i]);
+		}
+		
+		for(i=0;i<=array.length;i++) {
+			
+		}
+
+	}
+
+}
